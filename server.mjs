402c5cbd91bfcf061b01b913a152534032d717a5
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-03-10 13:02:46 +0700

    [DB] - Connect to mongoDB

--- server.mjs
+++ server.mjs
@@ -1,69 +1,21 @@
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import mongoose from 'mongoose';
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import fakeDdata from './fakeData/index.js'
-
-// The GraphQL schema
-const typeDefs = `#graphql
-    type Folder {
-        id: String,
-        name: String,
-        createdAt: String,
-        author: Author
-        note: [Note]
-    }
-
-    type Author {
-        id: String,
-        name: String,
-    }
-
-    type Note{
-        id: String,
-        content: String,
-    }
-
-    type Query {
-        authors:[Author],
-        folders: [Folder],
-        folder(folderId: String): Folder,
-        note(noteId: String): Note
-    }
-`;
-
-// A map of functions which return data for the schema.
-const resolvers = {
-    Query: {
-        authors: () => fakeDdata.authors,
-        folders: () => fakeDdata.folders,
-        folder: (parent, agrs) => {
-            const folderId = agrs.folderId
-            return fakeDdata.folders.find(folder => folder.id == folderId)
-        },
-        note: (parent, agrs) => {
-            const noteId = agrs.noteId
-            return fakeDdata.notes.find(note => note.id == noteId)
-        }
-    },
-    Folder: {
-        author: (parent, agrs) => {
-            const authorId = parent.authorId
-            return fakeDdata.authors.find(author => author.id == authorId)
-        },
-        note: (parent, agrs) => {
-            return fakeDdata.notes.filter(note => note.folderId === parent.id)
-        }
-    },
-
-};
+import 'dotenv/config'
+import { typeDefs } from './schemas/typDefs.js'
+import { resolvers } from './resolvers/resolvers.js';
 
 const app = express();
 const httpServer = http.createServer(app);
 
+//MongoDB
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster-note-app.25zm0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-note-app`
+const PORT = process.env.PORT || 4000
 // Set up Apollo Server
 const server = new ApolloServer({
     typeDefs,
@@ -78,5 +30,11 @@ app.use(
     expressMiddleware(server),
 );
 
-await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000`);
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(async () => {
+    await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+    console.log(`🚀 Server ready at http://localhost:4000`);
+})
+
